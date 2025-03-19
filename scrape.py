9740e3150ac53bc203be3514a2ce0da9eb1a145a
dassrikrishna <srikrishnadas319@gmail.com>
2025-03-19 12:11:54 +0530

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from zoneinfo import ZoneInfo
 from selenium.webdriver.common.by import By
 
 # Function to scrape news headlines, links, and time
@@ -15,3 +17,26 @@ def scrape(driver):
                     print("LINK:", href)
                     print("Time:", time.text)
                     print("~" * 100)
+
+# Extract the thumbnail and the headline
+def headline_thumbnail(driver):
+    articles = driver.find_elements(By.TAG_NAME, "article")
+    
+    for article in articles:
+        a_tags = article.find_elements(By.TAG_NAME, "a")
+        
+        for a_tag in a_tags:
+            link_text = a_tag.text
+            href = a_tag.get_attribute("href")
+            
+            if link_text and href:
+                thumbnail = article.find_element(By.TAG_NAME, "img")
+                
+                if thumbnail:
+                    thumbnail_url = thumbnail.get_attribute("src")
+                    
+                    if thumbnail_url and "api" in thumbnail_url:
+                        print("Headline:", link_text)
+                        print("Headline URL:", href)
+                        print("Thumbnail URL:", thumbnail_url)
+                        print("~" * 100)
