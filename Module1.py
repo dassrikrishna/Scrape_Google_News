aa62071a7da2d3a7fb0b2999ff04978086cd9bd1
dassrikrishna <srikrishnadas319@gmail.com>
2025-03-19 11:14:29 +0530

    Update Module1.py

--- Module1.py
+++ Module1.py
@@ -1,7 +1,9 @@
 # !pip install selenium
+"""Scrape the home page of Google News"""
+
 import os
+from scrape import scrape
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 
 # Set up the WebDriver path
 os.environ['PATH'] += r"C:\chromedriver"
@@ -9,24 +11,9 @@ os.environ['PATH'] += r"C:\chromedriver"
 # Initialize the Chrome WebDriver
 driver = webdriver.Chrome()
 
+# driver go through this link
 driver.get("https://news.google.com/home?hl=en-IN&gl=IN&ceid=IN:en")
 driver.implicitly_wait(30)
-
-# Function to scrape news headlines, links, and time
-def scrape(driver):
-    articles = driver.find_elements(By.TAG_NAME, "article")
-    for article in articles:
-        a_tags = article.find_elements(By.TAG_NAME, "a")
-        for a_tag in a_tags:
-            link_text = a_tag.text
-            href = a_tag.get_attribute("href")
-            if link_text and href:
-                time = article.find_element(By.TAG_NAME, "time")
-                if time:
-                    print("Headline:", link_text)
-                    print("LINK:", href)
-                    print("Time:", time.text)
-                    print("~" * 100)
 
 # Call the scraping function
 scrape(driver)
